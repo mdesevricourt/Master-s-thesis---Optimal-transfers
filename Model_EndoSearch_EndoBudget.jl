261b3900802ff66f82de87620d4c68f136192bd1
Maxime Cugnon de Sévricourt <m.desevricourt@hotmail.fr>
2018-05-31 18:24:06 +0200

    created a new file with various functions, namely Welfare, budget, netwage and tauofT

--- Model_EndoSearch_EndoBudget.jl
+++ Model_EndoSearch_EndoBudget.jl
@@ -1,97 +1,35 @@
 using Optim
-using Plots
+using Plots, gr()
 using Distributions
 #using JLD2
 #using LaTeXStrings
 #push!(LOAD_PATH, "C:/Users/Maxime/.julia/v0.6/")
 using utilityfunctions
 using Searcheffort
-
-gr()
-Default = Dict("n_e" => 0.5, "p_e|e" => 0.9, "p_u|u" => 0.9, "wage" => 600.0, "home production" => 50, "Budget" => 300.0, "Market tightness" => 0.8)
-Default["tax rate"] = 0.2
-Default["net wage"] = Default["wage"] * (1 - Default["tax rate"])
+using otherfucntions #welfare, budget, and τofT functions
 
 
-function Welfare(T::Real, τ::Real , e::Real = Default["n_e"], p_e_e::Real = Default["p_e|e"], p_u_u::Real = Default["p_u|u"], wb::Real = Default["wage"], wn::Real = Default["net wage"], δ::Real = Default["home production"]; λ::Real =Default["Market tightness"])
-    ω = weights(e, p_e_e, p_u_u, λ = λ)
-    return(ω[1] * u(wn + T) + ω[2] * u(wb + T + τ) + ω[3] * u(δ + T) + ω[4] * u(δ + T + τ) - disut(e))
-end
-
-function budget(;ρ::Real = Default["tax rate"], gw::Real = Default["wage"], e::Real = 1, p_e_e::Real = Default["p_e|e"])
-    return(ρ * gw * e * p_e_e)
-end
-
-function budgetcons(T::Real, τ::Real; gw = Default["wage"], nw = Default["net wage"], ρ::Real = Default["tax rate"], n_e::Real = Default["n_e"], p_e_e::Real = Default["p_e|e"], p_u_u::Real = Default["p_u|u"], B::Real = Default["Budget"], λ = Default["Market tightness"])
-    ω = weights(n_e, p_e_e, p_u_u, λ = λ)
-    B = budget(ρ = ρ, gw = gw, e = n_e, p_e_e = p_e_e)
-    if (T + (ω[2] + ω[4]) * τ) > B
-        return(false)
-    else
-        return(true)
-    end
-end
-
-function τofT(T::Real; gw::Real = Default["wage"], ρ::Real = Default["tax rate"], nw::Real = Default["net wage"], δ::Real = Default["home production"], p_e_e = Default["p_e|e"], p_u_u = Default["p_u|u"], λ::Real  = Default["Market tightness"])
-    ω = weights(1, p_e_e, p_u_u, λ = λ)
-    τ = 0
-    counter = 0
-    lb = 0
-    ub = (budget(ρ = ρ, gw = gw, e = 1, p_e_e = p_e_e) - T)/ (ω[2] + ω[4] )
-    e = 0
-    B = 0
-    while ub - lb > 1
-        for t in [lb, (lb + ub)/2, ub]
-            counter += 1
-            e = Optimeffort(T, t; wb = gw, wn = nw , δ = δ, p_e_e = p_e_e, p_u_u = p_u_u, λ = λ )
-            B = budget(ρ = ρ, gw = gw, e = e, p_e_e = p_e_e)
-            ω = weights(e, p_e_e, p_u_u; λ = λ)
-            if t <= (B - T)/ (ω[2] + ω[4] )
-                lb = t
-                continue
-            else
-                ub = t
-                break
-            end
-        end
-    end
-    println(counter)
-    for t in Array(linspace(lb,ub,11))
-        counter += 1
-        e = Optimeffort(T, t;  wb = gw, wn = nw , δ = δ, p_e_e = p_e_e, p_u_u = p_u_u, λ = λ)
-        B = budget(ρ = ρ, gw = gw, e = e, p_e_e = p_e_e)
-        ω = weights(e, p_e_e, p_u_u; λ = λ)
-        if t <= (B - T)/ (ω[2] + ω[4] )
-            τ = t
-            continue
-        else
-            break
-        end
-    end
-    return([τ, Optimeffort(T, τ; wb = gw, wn = nw , δ = δ, p_e_e = p_e_e, p_u_u = p_u_u, λ = λ)])
-end
-
-
-
-function Ω(T::Real; p_e_e::Real = Default["p_e|e"], p_u_u::Real = Default["p_u|u"], B::Real = Default["Budget"], w::Real = Default["wage"], δ::Real = Default["home production"], λ = Default["Market tightness"])
-    τ, e = τofT(T, w = w , p_e_e = p_e_e, p_u_u = p_u_u, B = B, λ = λ)
-    if (T < 0) || (τ < 0) || !budgetcons(T, τ; n_e = e, p_e_e = p_e_e, p_u_u = p_u_u, B = B, λ = λ)
+function Ω(T::Real; p_e_e::Real = Default["p_e|e"], p_u_u::Real = Default["p_u|u"], B::Real = Default["Budget"], gw::Real = Default["wage"], δ::Real = Default["home production"], λ = Default["Market tightness"])
+    τ, e = τofT(T, gw = gw, ρ = ρ,  p_e_e = p_e_e, p_u_u = p_u_u, λ = λ)
+    if (T < 0) || (τ < 0) || !budgetcons(T, τ; n_e = e, p_e_e = p_e_e, p_u_u = p_u_u, λ = λ)
         return(- Inf)
     else
-        return(Welfare(T, τ, e, p_e_e, p_u_u, w, δ, λ = λ))
+        return(Welfare(T, τ; e = e, p_e_e = p_e_e, p_u_u = p_u_u, wb = gw, wn = nw, δ = δ, λ = λ))
     end
 end
 
 
 
-function Solve(; p_e_e::Real = Default["p_e|e"], p_u_u::Real = Default["p_u|u"], B::Real = Default["Budget"], w::Real = Default["wage"], δ::Real = Default["home production"], λ = Default["Market tightness"])
+function Solve(; p_e_e::Real = Default["p_e|e"], p_u_u::Real = Default["p_u|u"], w::Real = Default["wage"], δ::Real = Default["home production"], ρ::Real = Default["tax rate"], λ = Default["Market tightness"])
     Param = Dict{String, Real}(
     "p_e|e" => p_e_e,
     "p_u|u" => p_u_u,
     "wage" => w,
     "home production" => δ,
     "Budget" => float(B),
-    "Market tightness" => λ)
+    "Market tightness" => λ,
+    "tax rate" => ρ)
+    Param["net wage"] = Param["wage"] * (1 -  Param["tax rate"])
 
     function Obj(T::Real)
         return(- Ω(T,  p_e_e = Param["p_e|e"], p_u_u = Param["p_u|u"], B = Param["Budget"], w = Param["wage"], δ = Param["home production"], λ = λ))
---
+++ Welfare + Budget + tau functions.jl
@@ -0,0 +1,73 @@
+module otherfunctions
+using utilityfunctions, Optimeffort
+export Welfare, budgetcons, budgetplot, τofT, netwage
+
+Default = Dict("n_e" => 0.5, "p_e|e" => 0.9, "p_u|u" => 0.9, "wage" => 600.0, "home production" => 50, "Budget" => 300.0, "Market tightness" => 0.8)
+Default["tax rate"] = 0.2
+
+function netwage(gw::Real = Default["wage"] , ρ::Real = Default["tax rate"])
+    return(gw * (1- ρ))
+end
+
+function Welfare(T::Real, τ::Real; e::Real = Default["n_e"], p_e_e::Real = Default["p_e|e"], p_u_u::Real = Default["p_u|u"], wb::Real = Default["wage"], ρ::Real = Default["tax rate"] , δ::Real = Default["home production"]; λ::Real =Default["Market tightness"])
+    wn = netwage(wb, ρ)
+    ω = weights(e, p_e_e, p_u_u, λ = λ)
+    return(ω[1] * u(wn + T) + ω[2] * u(wb + T + τ) + ω[3] * u(δ + T) + ω[4] * u(δ + T + τ) - disut(e))
+end
+
+function budget(;ρ::Real = Default["tax rate"], gw::Real = Default["wage"], e::Real = 1, p_e_e::Real = Default["p_e|e"])
+    return(ρ * gw * e * p_e_e)
+end
+
+function budgetcons(T::Real, τ::Real; gw = Default["wage"], nw = Default["net wage"], ρ::Real = Default["tax rate"], n_e::Real = Default["n_e"], p_e_e::Real = Default["p_e|e"], p_u_u::Real = Default["p_u|u"], λ = Default["Market tightness"])
+    ω = weights(n_e, p_e_e, p_u_u, λ = λ)
+    B = budget(ρ = ρ, gw = gw, e = n_e, p_e_e = p_e_e)
+    if (T + (ω[2] + ω[4]) * τ) > B
+        return(false)
+    else
+        return(true)
+    end
+end
+
+function τofT(T::Real; gw::Real = Default["wage"], ρ::Real = Default["tax rate"], δ::Real = Default["home production"], p_e_e = Default["p_e|e"], p_u_u = Default["p_u|u"], λ::Real  = Default["Market tightness"])
+    nw = netwage(gw, ρ)
+    ω = weights(1, p_e_e, p_u_u, λ = λ)
+    τ = 0
+    counter = 0
+    lb = 0
+    ub = (budget(ρ = ρ, gw = gw, e = 1, p_e_e = p_e_e) - T)/ (ω[2] + ω[4] )
+    e = 0
+    B = 0
+    while ub - lb > 1
+        for t in [lb, (lb + ub)/2, ub]
+            counter += 1
+            e = Optimeffort(T, t; wb = gw, wn = nw , δ = δ, p_e_e = p_e_e, p_u_u = p_u_u, λ = λ )
+            B = budget(ρ = ρ, gw = gw, e = e, p_e_e = p_e_e)
+            ω = weights(e, p_e_e, p_u_u; λ = λ)
+            if t <= (B - T)/ (ω[2] + ω[4] )
+                lb = t
+                continue
+            else
+                ub = t
+                break
+            end
+        end
+    end
+    println(counter)
+    for t in Array(linspace(lb,ub,11))
+        counter += 1
+        e = Optimeffort(T, t;  wb = gw, wn = nw , δ = δ, p_e_e = p_e_e, p_u_u = p_u_u, λ = λ)
+        B = budget(ρ = ρ, gw = gw, e = e, p_e_e = p_e_e)
+        ω = weights(e, p_e_e, p_u_u; λ = λ)
+        if t <= (B - T)/ (ω[2] + ω[4] )
+            τ = t
+            continue
+        else
+            break
+        end
+    end
+    return([τ, Optimeffort(T, τ; wb = gw, wn = nw , δ = δ, p_e_e = p_e_e, p_u_u = p_u_u, λ = λ)])
+end
+
+
+end
